f29fe5638921da4d5912bd08da696a678a8d37d3
Marco Leskovar <leskovarmarco@gmail.com>
2023-06-12 22:33:59 -0300

    header v1.2 || parallex main || cambios minimos en subpages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const hamburger = document.querySelector (".index-header__div--hamburger")
-const supagesList = document.querySelector (".index-header__navbar")
+const hamburger = document.querySelector ('.index-header__right--hamburger')
+const supagesList = document.querySelector ('.index-header__navbar')
+const subpagesListElements = document.querySelectorAll ('.index-header__navbar--subpages--list--elements')
 
 hamburger.onclick = () =>{
     hamburger.classList.toggle('active');
@@ -10,3 +11,13 @@ document.querySelectorAll(".index-header__navbar--subpages--list--elements").for
     hamburger.classList.remove('active')
     supagesList.classList.remove('active')
 })
+
+window.onscroll = () =>{
+    const posicionScroll = window.scrollY;
+    const metanoiaTexto = document.querySelector('.index-main__div--text');
+    const metanoiaMariposa = document.querySelector('.index-main__div--img');
+    const velocidadTexto = 0.6;
+    const velocidadImagen = 0.1;
+    metanoiaTexto.style.transform = `translateY(${posicionScroll * velocidadTexto}px)`;
+    metanoiaMariposa.style.transform = `translateY(${posicionScroll * velocidadImagen}px)`;
+  };
