949fc8cda037851b1517a3fbc0c40d2925f8f1e5
Marco Leskovar <leskovarmarco@gmail.com>
2023-06-27 18:04:26 -0300

    agregar productos por cantidad desde selected_product

--- js/funciones.js
+++ js/funciones.js
@@ -97,6 +97,45 @@ const verProducto = (id) =>{
 //VER PRODUCTO EN DETALLE = END
 
 
+//INPUT DE CANTIDAD EN ELEGIDO = START
+const agregarPorCantidad = (id) =>{
+    const inputCantidad = document.getElementById(`agregarPorCantidad-${id}`)
+    const valorInput = parseInt(inputCantidad.textContent)
+    return valorInput
+}
+//INPUT DE CANTIDAD EN ELEGIDO = END
+
+
+//CREAR LISTA DE CANTIDAD = START
+const listaDeCantidadCrear = (id, cantidad) =>{
+    const contenedor = document.getElementById(`listaAgregarCantidad-${id}`);
+    let contenido = '';
+    for (let i = 1; i <=5; i++){
+        contenido += `<li class="selected-main__sect--info--lista--ul--li" id="elementoNumero${i}" onclick="seleccionarLaCantidad(${id}, ${i})">${i} unidad${i > 1 ? 'es' : ''}</li>`;
+    }
+    contenedor.innerHTML = contenido;
+    const lista = document.getElementById(`listaCantidadProducto`)
+    const botonCantidad = document.getElementById(`agregarPorCantidad-${id}`)
+    const arrow = document.getElementById(`arrowCantidad`)
+    botonCantidad.classList.toggle('active');
+    lista.classList.toggle('active');
+    arrow.classList.toggle('active')
+}
+//CREAR LISTA DE CANTIDAD = END
+
+
+//APLICAR CANTIDAD AL BOTON Y CERRAR LISTA = START
+const seleccionarLaCantidad = (id, cantidad) =>{
+    const botonCantidad = document.getElementById(`agregarPorCantidad-${id}`)
+    botonCantidad.innerHTML = cantidad == 1 ? `${cantidad} unidad` : `${cantidad} unidades`;
+    const lista = document.getElementById(`listaCantidadProducto`);
+    lista.classList.remove('active');
+    const arrow = document.getElementById(`arrowCantidad`)
+    arrow.classList.remove('active')
+}
+//APLICAR CANTIDAD AL BOTON Y CERRAR LISTA = END
+
+
 //RENDERIZAR PRODUCTO ELEGIDO = START
 const renderProduct = () =>{
     const seleccionado = JSON.parse(localStorage.getItem('seleccionado'))
@@ -106,7 +145,17 @@ const renderProduct = () =>{
 <div class="selected-main__sect--info">
     <h1 class="selected-main__sect--info--name">${seleccionado.producto}</h1>
     <h2 class="selected-main__sect--info--price">${'$' + seleccionado.precio}</h2>
-    <button class="selected-main__sect--info--add" onclick="agregarProducto(${seleccionado.id})" id="agregarCarrito">
+    <div class="selected-main__sect--info--cantidad">
+        <button class="selected-main__sect--info--cantidad--input" id='agregarPorCantidad-${seleccionado.id}' onclick="listaDeCantidadCrear(${seleccionado.id}, 1)">1 unidad 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="selected-main__sect--info--cantidad--input--arrow bi bi-chevron-down" viewBox="0 0 16 16" id="arrowCantidad">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </button>
+    </div>
+    <div class="selected-main__sect--info--lista" id='listaCantidadProducto'>
+        <ul class="selected-main__sect--info--lista--ul" id='listaAgregarCantidad-${seleccionado.id}'></ul>
+    </div>
+    <button class="selected-main__sect--info--add" onclick="agregarProducto(${seleccionado.id}, agregarPorCantidad(${seleccionado.id}))" id="agregarCarrito">
         Agregar al carrito
         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
@@ -141,17 +190,15 @@ const estaEnElCarrito = (id) =>{
 
 
 //GUARDAR PRODUCTO DE CARRITO = START
-const agregarProducto = (id) =>{
+const agregarProducto = (id, cantidad) =>{
     const carrito = importarCarrito ()
     const productos = cargarProducto()
-    /* const inputCantidad = document.getElementById("inputCantidad");
-    const cantidad = parseInt(inputCantidad.value); */
     if (estaEnElCarrito(id)){
         let posicion = carrito.findIndex((item) => item.id === id)
-        carrito[posicion].cantidad += 1
+        carrito[posicion].cantidad += cantidad
     }else{
         const agregado = productos.find((elemento) => elemento.id == id)
-        agregado.cantidad = 1
+        agregado.cantidad = cantidad
         carrito.push(agregado)
     }
     guardarCarrito(carrito)
--- js/productos.js
+++ js/productos.js
@@ -1,17 +1,17 @@
 //Creo array de productos
 const productos = [
-    {id: 1, producto: 'Abstract Black Shirt', precio: 3000, imagen: '../img/prod-1.webp'},
-    {id: 2, producto: 'Simple Orange Shirt', precio: 1000, imagen: '../img/prod-2.webp'},
-    {id: 3, producto: 'Simple Brown Shirt', precio: 980, imagen: '../img/prod-3.webp'},
-    {id: 4, producto: 'Marble Black & White Shirt', precio: 3500, imagen: '../img/prod-4.webp'},
-    {id: 5, producto: 'Abstract White Shirt', precio: 2900, imagen: '../img/prod-5.webp'},
-    {id: 6, producto: 'Hoodie Peace', precio: 8000, imagen: '../img/prod-6.webp'},
-    {id: 7, producto: 'Hoodie Skeleton', precio: 8200, imagen: '../img/prod-7.webp'},
-    {id: 8, producto: 'Fuck Off Shirt', precio: 3000, imagen: '../img/prod-8.webp'},
-    {id: 9, producto: 'Cargo Black', precio: 4500, imagen: '../img/prod-9.webp'},
-    {id: 10, producto: 'Cargo Style Purple', precio: 5000, imagen: '../img/prod-10.webp'},
-    {id: 11, producto: 'Cargo Style Blue', precio: 5000, imagen: '../img/prod-11.webp'},
-    {id: 12, producto: 'Cargo Black Chains', precio: 4500, imagen: '../img/prod-12.webp'},
+    {id: 1, producto: 'Abstract Black Shirt', precio: 3000, imagen: '../img/prod-1.webp', stock: 10},
+    {id: 2, producto: 'Simple Orange Shirt', precio: 1000, imagen: '../img/prod-2.webp' ,stock: 21},
+    {id: 3, producto: 'Simple Brown Shirt', precio: 980, imagen: '../img/prod-3.webp',stock: 4},
+    {id: 4, producto: 'Marble Black & White Shirt', precio: 3500, imagen: '../img/prod-4.webp',stock: 20},
+    {id: 5, producto: 'Abstract White Shirt', precio: 2900, imagen: '../img/prod-5.webp' ,stock: 11},
+    {id: 6, producto: 'Hoodie Peace', precio: 8000, imagen: '../img/prod-6.webp' ,stock: 14},
+    {id: 7, producto: 'Hoodie Skeleton', precio: 8200, imagen: '../img/prod-7.webp',stock: 3},
+    {id: 8, producto: 'Fuck Off Shirt', precio: 3000, imagen: '../img/prod-8.webp',stock: 0},
+    {id: 9, producto: 'Cargo Black', precio: 4500, imagen: '../img/prod-9.webp',stock: 2},
+    {id: 10, producto: 'Cargo Style Purple', precio: 5000, imagen: '../img/prod-10.webp',stock: 3},
+    {id: 11, producto: 'Cargo Style Blue', precio: 5000, imagen: '../img/prod-11.webp',stock: 4},
+    {id: 12, producto: 'Cargo Black Chains', precio: 4500, imagen: '../img/prod-12.webp',stock: 17},
 ]
 
 guardarProducto()//SE GUARDAN LOS PRODUCTOS EN STORAGE
