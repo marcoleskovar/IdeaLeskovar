f9602648ce35a948b1b02216244a132b5d6f1ac1
Marco Leskovar <leskovarmarco@gmail.com>
2023-07-07 15:41:46 -0300

    loaderCarrito

--- js/carrito.js
+++ js/carrito.js
@@ -26,7 +26,7 @@ const renderCarrito = () =>{
                 </div>
             </div>
         </div>
-        <svg class="carrito-main__sect--list--product--trash" onclick="eliminarProducto(${elegido.id})" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+        <svg class="carrito-main__sect--list--product--trash" onclick="loaderCarrito(${elegido.id})" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
         </svg>
@@ -61,7 +61,7 @@ const renderCarrito = () =>{
     }
 }
 
-eliminadoArray = []
+let toastClicked = false;
 const eliminarProducto = (id) =>{
     const carrito = importarCarrito()
     const elegido = carrito.filter((item) => item.id != id)
@@ -77,9 +77,12 @@ const eliminarProducto = (id) =>{
         stopOnFocus: true,
         className: `productoEliminado-${id}`,
         onClick: () => {
-            toast.hideToast();
-            recuperarEliminado(id);
-            renderCarrito()
+            if(!toastClicked){
+                toastClicked = true
+                toast.hideToast();
+                recuperarEliminado(id);
+                renderCarrito()
+            }
         },
         offset: {
             x: 0,
@@ -90,28 +93,17 @@ const eliminarProducto = (id) =>{
             background: "rgba(153, 0, 0, 0.7)",
         },
     }).showToast();
-    eliminadoArray.push(eliminado)
-    localStorage.setItem('eliminado', JSON.stringify(eliminadoArray))
+    localStorage.setItem('eliminado', JSON.stringify(eliminado))
 }
 
-const recuperarEliminado = (id) => {
-    const arrayEliminados = JSON.parse(localStorage.getItem('eliminado'));
+const recuperarEliminado = () => {
     const carrito = importarCarrito();
-    const alerta = document.getElementsByClassName(`productoEliminado-${id}`)
-    carrito.length === 0 && alerta && location.reload();
-    for(const producto of arrayEliminados){
-        if (producto.id === id) {
-            if (estaEnElCarrito(id)){
-                badgeCarrito()
-                renderCarrito();
-            }else{
-                carrito.push(producto);
-                guardarCarrito(carrito);
-                badgeCarrito()
-                return renderCarrito();
-            }
-        }
-    }
+    const recuperado =  JSON.parse(localStorage.getItem('eliminado'))
+    carrito.length === 0 && location.reload();
+    carrito.push(recuperado);
+    guardarCarrito(carrito);
+    badgeCarrito()
+    renderCarrito();
 }
 
 const precioProductosTotal = () =>{
@@ -131,5 +123,26 @@ const comprarCarrito = () =>{
     badgeCarrito()
     renderCarrito()
 }
+
+const loaderCarrito = (id) => {
+    const loader = document.getElementById('loaderCarrito');
+    loader.classList.add('active');
+  
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (loader){
+                loader.classList.remove('active');
+                eliminarProducto(id);
+                toastClicked = false;
+                resolve();
+            }else{
+                setTimeout(() => {
+                    loader.classList.remove('active');
+                    location.reload();
+            },5000);
+            }
+        },500);
+    });
+};
 
 renderCarrito()
